48846a43b845a3f5d85347eb5f2dbf884d7f8932
Christoph Kappel <christoph@unexist.dev>
2024-12-26 14:46:35 +0100

    Updated license

--- src/lang.c
+++ src/lang.c
@@ -5,8 +5,8 @@
  * @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
  * @version $Id$
  *
- * This program can be distributed under the terms of the GNU GPLv2.
- * See the file COPYING for details.
+ * This program can be distributed under the terms of the Apache License.
+ * See the file LICENSE for details.
  **/
 
 #include <strings.h>
--- src/lang.h
+++ src/lang.h
@@ -1,13 +1,13 @@
- /**
-  * @package showcase-asciidoxy
-  *
-  * @file lang.h
-  * @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
-  * @version $Id$
-  *
-  * This program can be distributed under the terms of the GNU GPLv2.
-  * See the file COPYING for details.
-  **/
+/**
+ * @package showcase-asciidoxy
+ *
+ * @file lang.h
+ * @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License.
+ * See the file LICENSE for details.
+ **/
 
 #ifndef LANG_H
 #define LANG_H 1
